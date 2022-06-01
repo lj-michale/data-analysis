d0af026c671923851e0bfb5884ff9838ceb733d9
LJMichale <jie.luo2@ctg.cn>
2022-06-01 10:06:53 +0800

    feat: README.md

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade --ignore-insta
 员工账号：admin
 密码：admin123
 
+# tushare-token
+d199faaeae0b16cd75bec98a6e99eb1671f088f1ca760977db11066c
 
 xgboost
 
---
+++ 常见脚本/run_spark_pipeline.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-#
+
+# -------------------------------------------------------------------------------
+# Name:
+# Description:
+# Author:       lj.michale
+# Date:         2022/1/17
+# -------------------------------------------------------------------------------
+
+import os
+import time
+import subprocess
+import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
